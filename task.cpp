68d89e1cd583f218ce77262e56271f37c1925155
rogue-wave <zhangjingjie@zju.edu.cn>
2025-11-19 20:24:53 +0800

    new branch 1

--- task.cpp
+++ task.cpp
@@ -2,13 +2,14 @@
  * @Author: rogue-wave zhangjingjie@zju.edu.cn
  * @Date: 2025-11-19 19:17:07
  * @LastEditors: rogue-wave zhangjingjie@zju.edu.cn
- * @LastEditTime: 2025-11-19 20:20:27
+ * @LastEditTime: 2025-11-19 20:24:50
  * @FilePath: \新建文件夹\task.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 #include <stdio.h>
 int main()
 {
+    printf("hello world");
     printf("hello world");
     printf("hello world");
 }
